ff130b6700c6205b3e2cc6e0f0f7aca19ab6dade
Syyren <129496054+Syyren@users.noreply.github.com>
2023-11-22 14:37:24 -0700

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    function bookSearch(){
+        var search = $('#search').val();
+
+        document.getElementById('results').innerHTML= ""
+
+        $.ajax ({
+            url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+            dataType: "json",
+            success: function(data){
+                var innerHTML = "";
+                for(var i=0; i < data.items.length; i++){
+                    if(data.items[i].volumeInfo.title && data.items[i].volumeInfo.imageLinks &&
+                        data.items[i].volumeInfo.imageLinks.smallThumbnail){
+                            innerHTML += "<span class='col-md-4' title=''>" + data.items[i].volumeInfo.title + "</span>"
+                            innerHTML += "<img class='col-md-4' src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + ">" + "</img>"
+                        }
+                }
+                $('#results').html(innerHTML);
+            },
+            type: 'GET'
+        });
+    }
+    $("#button").on("click", bookSearch);
+});
