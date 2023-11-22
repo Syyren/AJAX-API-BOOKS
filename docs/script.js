40621328be4cb1b195882d5bbbb07e36b328406c
Syyren <129496054+Syyren@users.noreply.github.com>
2023-11-22 14:46:01 -0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -13,8 +13,8 @@ $(document).ready(function(){
                 for(var i=0; i < data.items.length; i++){
                     if(data.items[i].volumeInfo.title && data.items[i].volumeInfo.imageLinks &&
                         data.items[i].volumeInfo.imageLinks.smallThumbnail){
-                            innerHTML += "<span class='col-md-4' title=''>" + data.items[i].volumeInfo.title + "</span>" + "<br>"
-                            innerHTML += "<img class='col-md-4' src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + ">" + "</img>" + "<br>"
+                            innerHTML += "<p class='col-md-4' title=''>" + data.items[i].volumeInfo.title + "</p>"
+                            innerHTML += "<img class='col-md-4' src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + ">" + "</img>";
                         }
                 }
                 $('#results').html(innerHTML);
